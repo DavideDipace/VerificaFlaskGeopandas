3f058a04fc0da377359873e81433f73c3d1c971b
DavideDipace <dipace.davide@iisgalvanimi.edu.it>
2023-05-16 10:12:05 +0000

    esercizio Flask

---
+++ esercizio1.py
@@ -0,0 +1,28 @@
+from flask import Flask, request, render_template
+import geopandas as gpd
+
+app = Flask(__name__)
+
+df = pd.read_excel("https://github.com/PolisenoRiccardo/perilPopolo/blob/main/milano_housing_02_2_23.xlsx?raw=true")
+df
+
+quartieri = gpd.read_file("ds964_nil_wm.zip") #caricare dati da drive
+quartieri
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    return render_template('home.html')
+
+@app.route('/tabella', methods=['GET', 'POST'])
+def tab():
+    return render_template('test.html', table = df.to_html())
+
+@app.route('/data', methods=['GET', 'POST'])
+def data():
+    quartiere = request.args.get('quartiere')
+    Quartiere = df[df['neighborhood'] == quartiere]
+    Quartiere_sort = Quartiere.sort_values("date")
+    return render_template('riepilogo.html', quartiere=Quartiere_sort.to_html())#per rappresentare una tabella usare il metodo .to_html()
+    
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=3245, debug=True)
